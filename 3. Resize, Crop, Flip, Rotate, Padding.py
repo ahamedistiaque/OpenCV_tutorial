90245d563f9b07628e1c391b9392ac57bd1c0e2a
Istiaque Ahamed <103270578+ahamedistiaque@users.noreply.github.com>
2026-02-01 08:39:59 +0100

    Image Transformations

---
+++ 3. Resize, Crop, Flip, Rotate, Padding.py
@@ -0,0 +1,31 @@
+import cv2
+
+img = cv2.imread('bird2.jpg')
+
+# Resize
+resized = cv2.resize(img, (720, 512))
+
+# Crop
+crop = resized[100:400, 100:400]
+
+# Flip
+flip_h = cv2.flip(resized, 1)
+
+# Rotate
+(h, w) = resized.shape[:2]
+center = (w//2, h//2)
+matrix = cv2.getRotationMatrix2D(center, 45, 1.0)
+rotated = cv2.warpAffine(resized, matrix, (w, h))
+
+# Padding
+padded = cv2.copyMakeBorder(resized, 50, 50, 50, 50,
+                            cv2.BORDER_CONSTANT, value=(0,0,0))
+
+cv2.imshow("Resized", resized)
+cv2.imshow("Cropped", crop)
+cv2.imshow("Flipped", flip_h)
+cv2.imshow("Rotated", rotated)
+cv2.imshow("Padded", padded)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
