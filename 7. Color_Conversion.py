0ab42909eb8939c755e6d1b66f7c2def2b343d15
Istiaque Ahamed <103270578+ahamedistiaque@users.noreply.github.com>
2026-02-01 09:10:12 +0100

    BGR, Gray, HSV

---
+++ 7. Color_Conversion.py
@@ -0,0 +1,21 @@
+import cv2
+
+# Read image
+img = cv2.imread('macaw.jpg')
+
+# Resize for easier display
+#img = cv2.resize(img, (500, 500))
+
+# Convert to Gray
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Convert to HSV
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Show images
+cv2.imshow("Original (BGR)", img)
+cv2.imshow("Gray", gray)
+cv2.imshow("HSV", hsv)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
