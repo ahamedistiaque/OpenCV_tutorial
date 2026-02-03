6dade9a5d689deadb44d778ae2dbbdde2f26001e
Istiaque Ahamed <103270578+ahamedistiaque@users.noreply.github.com>
2026-02-03 17:58:55 +0100

    Contours

---
+++ 10. Image Contours.py
@@ -0,0 +1,26 @@
+# Used to find and draw shapes/outlines of objects in an image
+
+import cv2
+
+# Read  image
+img = cv2.imread('bird.jpg')
+
+
+img = cv2.resize(img, (500, 500)) # Resize image
+
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply threshold
+_, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+# Find contours
+contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Draw contours on original image
+cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
+
+# Show image
+cv2.imshow("Contours", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
