753637e54fcb3e65f4140a010e3aa62d9dbe5ac7
Istiaque Ahamed <103270578+ahamedistiaque@users.noreply.github.com>
2026-02-10 19:34:47 +0100

    Reformat and clean up image contours code

--- 10. Image Contours.py
+++ 10. Image Contours.py
@@ -1,5 +1,6 @@
-# """Image contours in Python are detected using OpenCV (cv2), typically 
-involving loading an image, converting to grayscale, applying thresholding or edge detection (like Canny) to create a binary image, and using cv2.findContours().""" 
+ """Image contours in Python are detected using OpenCV (cv2), typically 
+involving loading an image, converting to grayscale, applying thresholding or edge 
+detection (like Canny) to create a binary image, and using cv2.findContours().""" 
 
 import cv2
 
@@ -25,5 +26,6 @@ cv2.drawContours(img, contours, -1, (0, 255, 0), 2)
 cv2.imshow("Contours", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
 
